be15e7070bdbe691dc9f2adf10bf3097e214a1f4
Rangoli.Saxena <rangolisaxena90@gmail.com>
2017-11-22 17:30:59 +0100

    days work

--- read_file.py
+++ read_file.py
@@ -39,16 +39,16 @@ def get_measured_data(file_path,start_index=0):
 
 np_sim_dose , np_sim_dist = get_sim_data('Lydia_txt', coord={'x':-1,'y':-1,'z':0})
 np_meas_dist, np_meas_dose = get_measured_data('MEAS_inplane_10x10_SSD100_depth16mm.txt')
-
-print(np_sim_dose)
-print(np_sim_dist)
-print(np_meas_dist)
-print(np_meas_dose)
-
+#
+# print(np_sim_dose)
+# print(np_sim_dist)
+# print(np_meas_dist)
+# print(np_meas_dose)
+#
 
 
 np_sim_dist = [float(ix)*10 for ix in np_sim_dist]
-print(np_sim_dist)
+# print(np_sim_dist)
 
 # np_sim_dict = dict(zip(np_sim_dist,np_meas_dose))
 # np_sim_dist =
@@ -56,13 +56,13 @@ print(np_sim_dist)
 np_sim_dist_clipped = [ix for ix in np_sim_dist if abs(ix) <82]
 np_sim_dose_clipped = np_sim_dose[np_sim_dist.index(np_sim_dist_clipped[0]):np_sim_dist.index(np_sim_dist_clipped[-1])+1]
 
-print(np_sim_dist_clipped)
-print(np_sim_dose_clipped)
-print('MANGO')
-print(len(np_meas_dose))
-print(len(np_sim_dose_clipped))
-print(len(np_meas_dist))
-print(len(np_sim_dist_clipped))
+# print(np_sim_dist_clipped)
+# print(np_sim_dose_clipped)
+# print('MANGO')
+# print(len(np_meas_dose))
+# print(len(np_sim_dose_clipped))
+# print(len(np_meas_dist))
+# print(len(np_sim_dist_clipped))
 
 def calc_gamma(sim_dose,sim_dist, meas_dose,meas_dist):
 
@@ -75,23 +75,25 @@ def calc_gamma(sim_dose,sim_dist, meas_dose,meas_dist):
     gamma = np.add(diff_dist,diff_dose)
     gamma = [ix**(0.5) for ix in gamma]
 
-    print(gamma)
-    print(len(gamma))
+    # print(gamma)
+    # print(len(gamma))
+    return gamma
 
 
 
 def linear_interpolate(sim_dist,sim_dose,meas_dist):
     print('ELEPHANT')
-    print(meas_dist)
+    # print(meas_dist)
     new_sim_dose = []
     new_sim_dist = []
     for inx in meas_dist:
         # print(sim_dist)
         # print(meas_dist[inx])
-        temp = [ix-inx for ix in sim_dist]
+        temp = [abs(ix-inx) for ix in sim_dist]
         # print(sim_dist)
         # print(meas_dist)
         # print(temp)
+
         closest_inx = temp.index(min(temp))
         a = sim_dose[closest_inx]
         x = sim_dist[closest_inx]
@@ -101,24 +103,76 @@ def linear_interpolate(sim_dist,sim_dose,meas_dist):
         else:
             b = sim_dose[-1]
             y = sim_dist[-1]
-        slope = (b-a)/(y-x)
-        #
-        # print('YAYYAA')
-        # print(x,y,a,b)
-        # print(slope)
-        sim_interpolated_meas = inx*slope
-        new_sim_dose = new_sim_dose + [sim_interpolated_meas]
-        new_sim_dist = new_sim_dist + [inx]
+
+
+        if y-x != 0:
+            slope = (b-a)/(y-x)
+
+            print(inx)
+            print('closest inx' + str(closest_inx))
+            print('slope' + str(slope))
+            #
+            # print('YAYYAA')
+            # print(x,y,a,b)
+            # print(slope)
+            sim_interpolated_meas = abs(inx)*abs(slope)
+            print('slope' + str(slope))
+            new_sim_dose = new_sim_dose + [sim_interpolated_meas]
+            new_sim_dist = new_sim_dist + [inx]
+
+        else:
+            new_sim_dose = new_sim_dose + [new_sim_dose[-1]]
+            new_sim_dist = new_sim_dist + [inx]
 
     new_sim_dist = np.around(new_sim_dist,decimals=2)
-    print(new_sim_dose)
+    print('MANGOOOOOO')
+    # print(new_sim_dose)
     print(len(new_sim_dose))
-    print(new_sim_dist)
-    print(len(new_sim_dist))
-    return new_sim_dist,new_sim_dose
+    # print(new_sim_dist)
+    # print(len(new_sim_dist))
+    return new_sim_dist,new_sim_dose , meas_dist
 
 
+def normalize_dose(np_dose, high=1.0, low=0.0):
+    # np_dose /= np.max(np.abs(np_dose), axis=0)
+    # # print('loooo')
+    # # print(np.max(np.abs(np_dose), axis=0))
+    # # print(np_dose)
+    # return np_dose
+    mins = np.min(np_dose, axis=0)
+    maxs = np.max(np_dose, axis=0)
+    rng = maxs - mins
+    print('rng:   ... ' + str(maxs))
+    print('rngffdsf:   ... ' + str(mins))
+    return high - (((high - low) * (maxs - np_dose)) / rng)
 
 
-new_sim_dist,new_sim_dose = linear_interpolate(np_sim_dist_clipped,np_sim_dose_clipped,np_meas_dist)
-calc_gamma(new_sim_dose,new_sim_dist,np_meas_dose,np_meas_dist)
+new_sim_dist,new_sim_dose,meas_dist = linear_interpolate(np_sim_dist_clipped,np_sim_dose_clipped,np_meas_dist)
+# print('HEY')
+# print(new_sim_dose)
+# np_meas_dose =normalize_dose(np_meas_dose)
+# print(np_meas_dose)
+# print('BLING')
+print(new_sim_dose)
+new_sim_dose = normalize_dose(new_sim_dose)
+print('BANANA')
+
+print(new_sim_dose)
+# print(np_meas_dose)
+
+# print(new_sim_dose)
+
+gamma = calc_gamma(new_sim_dose,new_sim_dist,np_meas_dose,meas_dist)
+
+
+import matplotlib.pyplot as plt
+
+fig,ax = plt.subplots()
+s = 121
+# print(new_sim_dist)
+# print(new_sim_dose)
+ax.scatter(new_sim_dist, new_sim_dose, color='r', s=s/10, alpha=.4)
+# ax.scatter(meas_dist, np_meas_dose, color='b', s=s/10, alpha=.4)
+# ax.scatter(meas_dist,gamma, color='g', s=s/3, marker='s', alpha=.4)
+plt.show()
+
